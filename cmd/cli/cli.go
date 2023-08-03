abaecefe6f67f37eb654c06023abe211e58ccd1f
James Kelty <glassrye@gmail.com>
2023-08-03 12:27:32 -0700

    more migrations to Cobra. Removing possible environment variable thing I didn't really like in the first place.

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -21,59 +21,7 @@ type config struct {
 	backup          bool
 }
 
-// getCli parses command line flags and returns a config object and a slive of flag.FlagSet
-/*func getCli() *config {
-	var c config
-	c.backup = false
-	c.track = false
-
-	fs := flag.NewFlagSet("backup", flag.ExitOnError)
-	fs.StringVar(&c.backupBucket, "bucket", "", "Specify the bucket name")
-	fs.StringVar(&c.backupPrefix, "prefix", "", "Specify the prefix, aka folder, as a location")
-
-	ft := flag.NewFlagSet("track", flag.ExitOnError)
-	ft.StringVar(&c.host, "host", "", "The hostname or IP addr of the database")
-	ft.StringVar(&c.user, "user", "", "The DB user string")
-	ft.StringVar(&c.pass, "pass", "", "The DB user password string")
-	ft.StringVar(&c.database, "db", "", "The DB name (where the tables be, yarr....")
-
-	fm := flag.NewFlagSet("files", flag.ExitOnError)
-	fm.StringVar(&c.match, "match", "", "File name match. Like a regex.")
-	fm.StringVar(&c.suffix, "suff", "", "The suffix of the file, e.g., .log or .txt")
-	fm.StringVar(&c.watchDir, "dir", "", "The directory to look in for the files in")
-
-	fe := flag.NewFlagSet("env", flag.ExitOnError)
-	fe.StringVar(&c.envFile, "env", "", "An optional environment file")
-
-	switch os.Args[1] {
-	case "backup":
-		err := fs.Parse(os.Args[2:])
-		if err != nil {
-			log.Fatalf("there was an error parsing arguments: %v", err)
-		}
-		c.backupProviders = flag.Args()
-		c.backup = true
-		return &c
-	case "track":
-		err := ft.Parse(os.Args[2:])
-		if err != nil {
-			log.Fatalf("there was an error parsing arguments: %v", err)
-		}
-		c.track = true
-		return &c
-	case "files":
-		err := fm.Parse(os.Args[2:])
-		if err != nil {
-			log.Fatalf("there was an error parsing arguments: %v", err)
-		}
-		return &c
-	default:
-		return &c
-	}
-	// return &c
-}
-*/
-
+// getCli parses command line flags and returns a config object that can be used for archiving files.
 func getCli() *config {
 	var c config
 
@@ -111,6 +59,8 @@ func getCli() *config {
 	cmdFiles.Flags().StringVar(&c.suffix, "suff", "", "The suffix of the file, e.g., .log or .txt")
 	cmdFiles.Flags().StringVar(&c.watchDir, "dir", "", "The directory to look in for the files in")
 
+	// I haven't decided if I want to actually do this yet. Seems like bullshit extra stuff to me
+	// but I could be wrong so I'm leaving this hear for the time being
 	/*var cmdEnv = &cobra.Command{
 		Use:   "env",
 		Short: "Env command",
